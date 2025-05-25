dc66bb1e809b3726227e1fe3f657de46c9becd6d
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-25 16:57:38 +0600

    Explored advanced printing and typecasting

---
+++ input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    char c;
+    int y = 65;
+
+    cin >> x >> c;
+    cout << x << endl
+         << (int)c << endl
+         << (char)y;
+
+    return 0;
+}
