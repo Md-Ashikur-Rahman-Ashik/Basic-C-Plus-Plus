47c1eeff1fb4be8ef39f7f0fef527be0142bfeb4
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-25 21:23:54 +0600

    Solving vowel consonant problem using switch case

---
+++ vowelConsonant.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char character;
+    cin >> character;
+
+    switch (character)
+    {
+    case 'a':
+        /* code */
+        cout << "It's a vowel";
+        break;
+    
+    case 'b'
+    default:
+        break;
+    }
+
+    return 0;
+}
