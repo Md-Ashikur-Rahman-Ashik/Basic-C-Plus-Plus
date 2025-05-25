ca113a18cccf7ce19c411ad94ebd0e983bb841f7
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-25 16:42:51 +0600

    Explored basic printing in cpp

--- print.cpp
+++ print.cpp
@@ -5,9 +5,9 @@ int main()
     // std :: cout << "Assalamu Alaikum";
     int x = 10;
     double y = 36.5;
-    float = 3.5;
+    float z = 3.5;
 
-    std :: cout << x << "\n" << y << std :: endl;
+    std :: cout << x << "\n" << y << std :: endl << z;
 
     return 0;
 }
