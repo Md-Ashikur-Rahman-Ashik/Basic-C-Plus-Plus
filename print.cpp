7e82ae1eac021b4f3acb96ddc970948df4753141
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-25 16:38:53 +0600

    Exploring printing output in c program

--- print.cpp
+++ print.cpp
@@ -4,8 +4,10 @@ int main()
 {
     // std :: cout << "Assalamu Alaikum";
     int x = 10;
+    double y = 36.5;
+    float = 3.5;
 
-    std :: cout << x;
+    std :: cout << x << "\n" << y << std :: endl;
 
     return 0;
 }
