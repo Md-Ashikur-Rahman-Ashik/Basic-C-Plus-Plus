165e4bccb45fadb1a36f3e57dbd22bf651f0a5fe
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-26 16:20:33 +0600

    Explored string in cpp

---
+++ string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // char stringArray[100];
+    // cin >> stringArray;
+    // cin.getline(stringArray, 100);
+
+    string inputString;
+    cin >> inputString;
+
+    cout << inputString;
+
+    return 0;
+}
